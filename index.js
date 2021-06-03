42197e087e8735dc7e3103ea5c7dc791e55b1432
Irina Pyak <ir.pyak@gmail.com>
2021-06-03 16:49:10 -0400

    add src folder with questions.js file, __tests__ folder with js files, and add intern, engineer questions to questions.js file

---
+++ index.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+const path = require('path');
+const inquirer = require('inquirer')
+const Engineer = require('./lib/Engineer.js');
+const Intern = require('./lib/Intern.js');
+const Manager = require('./lib/Manager.js');
+
+
+async function main () {
+    // htmlCardArray [];
+
+    // generate Manager
+        // ask questions
+        // render new manager based on object props
+        // push to htmlCard Array
+        // back to mainMenu
+
+    // generate Intern
+        // ask questions
+        // render new manager based on object props
+        // push to htmlCard Array
+        // back to mainMenu
+
+    // generate Engineer
+        // ask questions
+        // render new manager based on object props
+        // push to htmlCard Array
+        // back to mainMenu
+        
+    // mainMenu questions
+        //generate Engineer
+        // generate Intern
+        // bye
+
+    //create a manager
+        // ask 
+        // generate html string from answers, and push to complete team
+        //ask for
+        // switch
+            // intern
+            // engineer
+            // done
+    //
+}
+
+// 🚀 start
+
+main();
+// Question objects contain questions for prompts
+
+
+
+// Add manger function
+
+//switch case function
+    // who to add? Intern? Engineer? Done?
+
+// Add Intern function
+
+// Add Engineer function
+
+// Build html function
---
+++ src/questions.js
@@ -0,0 +1,72 @@
+module.exports = {
+managerQuestions: [
+    {
+      type: "input",
+      name: "nameManager",
+      message: "Please enter your Manager name.",
+    },
+    {
+      type: "input",
+      name: "idManager",
+      message: "Please enter your Manager ID.",
+    },
+    {
+      type: "input",
+      name: "emailManager",
+      message: "Please enter your Manager email.",
+    },
+    {
+      type: "input",
+      name: "officeNumberManager",
+      message: "Please enter your Manager office number.",
+    }
+  ],
+
+    internQuestions: [
+    {
+      type: "input",
+      name: "nameIntern",
+      message: "Please enter your Intern name.",
+    },
+    {
+      type: "input",
+      name: "idIntern",
+      message: "Please enter your Intern ID.",
+    },
+    {
+      type: "input",
+      name: "emailIntern",
+      message: "Please enter your Intern email.",
+    },
+    {
+      type: "input",
+      name: "schoolIntern ",
+      message: "Please enter your Intern school.",
+    }
+  ],
+
+  engineerQuestions: [
+    {
+      type: "input",
+      name: "nameEngineer",
+      message: "Please enter your Engineer name.",
+    },
+    {
+      type: "input",
+      name: "idEngineer",
+      message: "Please enter your Engineer ID.",
+    },
+    {
+      type: "input",
+      name: "emailEngineer",
+      message: "Please enter your Engineer email.",
+    },
+    {
+      type: "input",
+      name: "githubEngineer",
+      message: "Please enter your Engineer github.",
+    }
+  ]
+
+
+}
