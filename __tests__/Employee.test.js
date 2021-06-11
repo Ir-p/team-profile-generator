9824fc2b5e780734dfe802285069435fdb2b71bf
Irina Pyak <ir.pyak@gmail.com>
2021-06-10 23:47:58 -0400

    edit test in Employee.test.js folder

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -15,7 +15,7 @@ describe('Employee Class Test', () => {
 
     // test success(properties, methods)
     describe('Employee Props', () => {
-        test('Name Prop', () => {
+        test('should return a string if a string is entered as an input', () => {
             // arrange
             const expectedNameValue = "Joanna"
 
@@ -25,7 +25,21 @@ describe('Employee Class Test', () => {
             // assert
            expect(employeeObj.name).toBe(expectedNameValue)
 
-        })
-    })
+        });
+
+        test('should return a number if a number is entered as input', () => {
+            const num = 123;
+            const employeeObj = new Employee("Joanna", num);
+            expect(employeeObj.id).toBe(num)
+        });
+
+        test('should return an email if an email entered as input', () => {
+            const email = "joanna@gmail.com";
+            const employeeObj = new Employee("Joanna", 123, email);
+            expect(employeeObj.email).toBe(email)
+        });
+    });
+
+
     // test failures, undefined, errors
 })
